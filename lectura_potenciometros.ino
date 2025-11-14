db2a0c69df2ade2f1d71a330188699ac8c287c39
Bruce Ossandon <bruce.inostroza@userena.cl>
2025-11-14 00:27:29 +0000

    agregados los dos potenciometros restantes

--- lectura_potenciometros.ino
+++ lectura_potenciometros.ino
@@ -7,9 +7,15 @@ void loop() {
   int p2 = analogRead(A1);
   int p3 = analogRead(A2);
   int p4 = analogRead(A3);
+  int p5 = analogRead(A4);
+  int p6 = analogRead(A5);
+
   Serial.print(p1); Serial.print(",");
   Serial.print(p2); Serial.print(",");
   Serial.print(p3); Serial.print(",");
-  Serial.println(p4);
+  Serial.print(p4); Serial.print(",");
+  Serial.print(p5); Serial.print(",");
+  Serial.println(p6);
+
   delay(20);
 }
